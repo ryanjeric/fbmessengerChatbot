413f5f46cdaee72d0075cb8a981c19212116e957
ryanjericsabado <ryanjericsabado@yahoo.com>
2020-09-20 22:38:50 +0800

    Update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -6,26 +6,26 @@ let postWebhook = (req, res) => {
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
 
-        body.entry.forEach(function(entry) {
+        body.entry.forEach(function (entry) {
 
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-          
-          
+
+
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
-          
+
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
-              handleMessage(sender_psid, webhook_event.message);        
+                handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
-              handlePostback(sender_psid, webhook_event.postback);
+                handlePostback(sender_psid, webhook_event.postback);
             }
-            
-          });
+
+        });
 
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
@@ -71,6 +71,34 @@ function handleMessage(sender_psid, received_message) {
         // Create the payload for a basic text message
         response = {
             "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
 
